b01ed0e8b52f743dce1ea7154356d8ca40aaa465
Jonatas Chagas <jonataschagas@gmail.com>
2016-01-26 15:36:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+#Lang Adventure Backend
+
+# Deploying function/endpoint
+
+Go to the function folder and run:
+``
+    serverless function deploy
+    serverless endpoint deploy
+``
+
+# Deploying resources (policies, etc):
+
+Go to the project resources and execute:
+``
+    serverless resources deploy
+``
+
+# More documentation here:
+http://docs.serverless.com/docs/commands-overview
---
+++ nodejsbackend/client_version/create/handler.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var ServerlessHelpers = require('serverless-helpers-js').loadEnv();
+var AWS = require('aws-sdk');
+
+// Require Logic
+var lib = require('../../lib');
+
+// Lambda Handler
+module.exports.handler = function(event, context) {
+    
+    // logging event
+    console.log(JSON.stringify(event));
+    
+    var alias = event['body']['alias'];
+    var active = event['body']['active'];
+    var createdDate = new Date();
+    
+    var dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+    
+    if(!alias) {
+        console.log('Error creating client version. Please provide and alias');
+        context.done(null, {'success': false, 'message': 'Error creating client version. Please provide an alias.'});
+        return
+    }
+    
+    var insertRecord = {
+        TableName: 'ClientVersion',
+        Item: {
+            'alias': alias,
+            'active': active,
+            'createdDate': createdDate.toDateString()
+        }
+    };
+    
+    console.log('creating new client version: '  + alias );
+    dynamoDbClient.put(insertRecord, function(err, data) {
+        if (err) {
+            console.log('Error creating client version: ' + err);
+            context.done(null, {'success': false, 'message': 'Error creating client version'});
+        } else {
+            console.log('Client version created successfully.');
+            context.done(null,{'success': true, 'message': 'Client version registered successfully.'});
+        }
+    });
+    
+};
