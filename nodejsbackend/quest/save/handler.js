2345dfced4bcac4d0a0cc8f20b362f9e7db7c401
jonataschagas <jonataschagas@gmail.com>
2016-02-21 20:04:35 +0200

    Bugfix in the quest/save method.

--- nodejsbackend/quest/save/handler.js
+++ nodejsbackend/quest/save/handler.js
@@ -15,16 +15,16 @@ module.exports.handler = function (event, context) {
 
     utils.log('saving quest: ', event);
 
-    var id = event.id, questOrder = event.questOrder,
-        storyId= event.storyId, title = event.title,
+    var id = event.id, questOrder = parseInt(event.questOrder),
+        storyId = event.storyId, title = event.title,
         introduction = event.introduction, introductionTranslation = event.introductionTranslation,
         completion = event.completion, completionTranslation = event.completionTranslation, now = new Date();
 
-    if (_.isEmpty(questOrder) || _.isEmpty(title) || _.isEmpty(storyId)) {
+    if (questOrder == null || _.isEmpty(title) || _.isEmpty(storyId)) {
         utils.error(
             context,
             'Quest',
-            'saved',
+            'saving',
             'Please provide the following parameters in ' +
             'order to save/update the quest: questOrder, title and story id.',
             null
@@ -32,45 +32,45 @@ module.exports.handler = function (event, context) {
         return;
     }
 
-    // first we check if there is a quest assigned to this order
-    db.listByGSI(
-        'Quest',
-        'ID, ' +
-        'Title',
-        'QuestOrder',
-        questOrder
-    ).then(
-        function (response) {
-            if (response && response.Items.length > 0) {
-                utils.error(
-                    context,
-                    'Quest',
-                    'saved',
-                    'A Quest is already assigned to this order. ' +
-                    'Please check the order before registering a Quest.',
-                    null
-                );
-                return;
-            } else {
-                if (!_.isEmpty(id)) {
-                    db.update('Quest', id, {
-                        'LastUpdated': now.toDateString(),
-                        'QuestOrder': questOrder,
-                        'Title': title,
-                        'Introduction': introduction,
-                        'IntroductionTranslation': introductionTranslation,
-                        'Completion': completion,
-                        'CompletionTranslation': completionTranslation,
-                        'StoryId': storyId
-                    }).then(function (response) {
-                        utils.log('Quest has been updated successfully.', response);
-                        utils.success(context, 'Quest', 'updated', {});
-                    }).catch(function (e) {
-                        utils.error(
-                            context, 'Quest', 'updating',
-                            'Error updating the quest in the database.', e
-                        );
-                    });
+    if (!_.isEmpty(id)) {
+        db.update('Quest', id, {
+            'LastUpdated': now.toDateString(),
+            'QuestOrder': questOrder,
+            'Title': title,
+            'Introduction': introduction,
+            'IntroductionTranslation': introductionTranslation,
+            'Completion': completion,
+            'CompletionTranslation': completionTranslation,
+            'StoryId': storyId
+        }).then(function (response) {
+            utils.log('Quest has been updated successfully.', response);
+            utils.success(context, 'Quest', 'updated', {});
+        }).catch(function (e) {
+            utils.error(
+                context, 'Quest', 'updating',
+                'Error updating the quest in the database.', e
+            );
+        });
+    } else {
+        // first we check if there is a quest assigned to this order
+        db.listByGSI(
+            'Quest',
+            'ID, ' +
+            'Title',
+            'QuestOrder',
+            questOrder
+        ).then(
+            function (response) {
+                if (response && response.Items.length > 0) {
+                    utils.error(
+                        context,
+                        'Quest',
+                        'saved',
+                        'A Quest is already assigned to this order. ' +
+                        'Please check the order before registering a Quest.',
+                        null
+                    );
+                    return;
                 } else {
                     db.save('Quest', {
                         'ID': db.guid(),
@@ -97,6 +97,6 @@ module.exports.handler = function (event, context) {
                     });
                 }
             }
-        }
-    );
+        );
+    }
 };
